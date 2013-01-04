850027a17360d92f92cc536f4ecf5e5b158099b0
Mark McDonnell <mark.mcdx@gmail.com>
2013-01-04 10:10:36 +0000

    Remove potentially unnecessary code
    Removed code in the hope that it would become clearer for someone to figure out why it's not working

--- namespace-integralist.php
+++ namespace-integralist.php
@@ -1,8 +1,4 @@
 <?php
-    namespace Integralist\Lib1;
-    use Integralist\Lib1;
-
-    // Used for autoloading tests
     use Integralist\Lib1\MyClass as IN1;
     use Integralist\Lib2\MyClass as IN2;
 
@@ -15,12 +11,6 @@
         require_once $class;
     }
 
-    /*
-        `__autoload` is a magic method that loads newly instantiated classes at run time
-        When using namespaces things get more complicated.
-        Our Classes need to be in the same folder structure as the namespace they're assigned.
-        We still need the `use` operator to qualify our namespace (see line 5 above)
-     */
     $in1 = new IN1();
     echo $in1->WhoAmI() . '<br><br>';
 
